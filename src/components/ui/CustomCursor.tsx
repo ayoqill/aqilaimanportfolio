b6eda1bec7658a73057fa5e5e3696883e24cccd4
aqil <aqilaiman058@gmail.com>
2026-02-20 12:25:46 +0800

    upgrade cursor

--- src/components/ui/CustomCursor.tsx
+++ src/components/ui/CustomCursor.tsx
@@ -7,7 +7,6 @@ export default function CustomCursor() {
   const [isDesktop, setIsDesktop] = useState(false);
 
   useEffect(() => {
-    // Detect touch device
     const hasTouch =
       "ontouchstart" in window || navigator.maxTouchPoints > 0;
 
@@ -30,32 +29,44 @@ export default function CustomCursor() {
       mouseY = e.clientY;
     };
 
-    const loop = () => {
-      posX += (mouseX - posX) * 0.12;
-      posY += (mouseY - posY) * 0.12;
+    const animate = () => {
+      // LOWER value = smoother & slower
+      posX += (mouseX - posX) * 0.07;
+      posY += (mouseY - posY) * 0.07;
 
       cursor.style.transform = `translate3d(${posX}px, ${posY}px, 0) translate(-50%, -50%)`;
-      requestAnimationFrame(loop);
+
+      requestAnimationFrame(animate);
     };
 
     window.addEventListener("mousemove", onMove, { passive: true });
-    requestAnimationFrame(loop);
+    requestAnimationFrame(animate);
 
     return () => {
       window.removeEventListener("mousemove", onMove);
     };
   }, [isDesktop]);
 
-  // Don't render on mobile/tablet
   if (!isDesktop) return null;
 
   return (
     <div
       ref={cursorRef}
-      className="fixed left-0 top-0 z-[9999] pointer-events-none transition-transform duration-150 ease-out"
+      className="fixed left-0 top-0 z-[9999] pointer-events-none"
     >
-      <svg width="22" height="22" viewBox="0 0 24 24">
+      <svg width="38" height="38" viewBox="0 0 24 24">
+        <defs>
+          <filter id="glow">
+            <feGaussianBlur stdDeviation="1.8" result="coloredBlur" />
+            <feMerge>
+              <feMergeNode in="coloredBlur" />
+              <feMergeNode in="SourceGraphic" />
+            </feMerge>
+          </filter>
+        </defs>
+
         <path
+          filter="url(#glow)"
           d="M12 2.7c1.2 0 2.2 1 2.2 2.2v2.2c0 .9.7 1.6 1.6 1.6h2.2c1.2 0 2.2 1 2.2 2.2s-1 2.2-2.2 2.2h-2.2c-.9 0-1.6.7-1.6 1.6v2.2c0 1.2-1 2.2-2.2 2.2s-2.2-1-2.2-2.2v-2.2c0-.9-.7-1.6-1.6-1.6H4.9c-1.2 0-2.2-1-2.2-2.2s1-2.2 2.2-2.2h2.2c.9 0 1.6-.7 1.6-1.6V4.9c0-1.2 1-2.2 2.2-2.2Z"
           fill="#39FF6A"
         />
